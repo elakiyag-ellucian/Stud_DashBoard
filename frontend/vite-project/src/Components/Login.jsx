bbc0ebe32d6c76569a9fdcee56a15d6c66a1eb97
elakiya <Elakiya.G@ellucian.com>
2025-02-25 17:55:31 +0530

    Login Page Started

---
+++ frontend/vite-project/src/Components/Login.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <>
+        <div className='Login_container'>
+          <div className='Login_img'>
+            <img src="ellucian-logo.png" alt="" className='Login_img'/>
+          </div>
+          <div className='Login_formdiv'>
+            <div className='Login_form'>
+              <input type="text" id="name" placeholder='Username'/>
+            </div>
+            <div className='Login_form'>
+              <input type="password" id="password" placeholder='Password'/>
+            </div>
+            <div>
+              <button className='Login_btn'>Submit</button>
+            </div>
+          </div>
+        </div>
+    </>
+  )
+}
+
+export default Login
