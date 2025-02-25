ddd2239c3ae06a3a2e63b15f598d5ac50a582bb8
elakiya <Elakiya.G@ellucian.com>
2025-02-25 20:38:59 +0530

    Login page css changed

--- frontend/vite-project/src/Components/Login.jsx
+++ frontend/vite-project/src/Components/Login.jsx
@@ -8,6 +8,7 @@ const Login = () => {
             <img src="ellucian-logo.png" alt="" className='Login_img'/>
           </div>
           <div className='Login_formdiv'>
+            <h1 className='Login_heading'>Login</h1>
             <div className='Login_form'>
               <input type="text" id="name" placeholder='Username'/>
             </div>
