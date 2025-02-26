38f99ededbe6d7585f686b7b511e7f0804aaa6ba
elakiya <Elakiya.G@ellucian.com>
2025-02-26 16:31:34 +0530

    html & ccs done for Login page

--- frontend/vite-project/src/Components/Login.jsx
+++ frontend/vite-project/src/Components/Login.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
+import { GoPersonFill } from "react-icons/go";
+import { RiLockPasswordFill } from "react-icons/ri";
 
 const Login = () => {
   return (
     <>
+    <div className='container'>
         <div className='Login_container'>
-          <div className='Login_img'>
-            <img src="ellucian-logo.png" alt="" className='Login_img'/>
+          <div className='Login_img_container'>
+            <img src="ellucian.jpg" alt="" className='Login_img' />
           </div>
           <div className='Login_formdiv'>
             <h1 className='Login_heading'>Login</h1>
             <div className='Login_form'>
+              <GoPersonFill />
               <input type="text" id="name" placeholder='Username'/>
             </div>
             <div className='Login_form'>
+              <RiLockPasswordFill />
               <input type="password" id="password" placeholder='Password'/>
             </div>
             <div>
@@ -20,6 +25,7 @@ const Login = () => {
             </div>
           </div>
         </div>
+    </div>
     </>
   )
 }
