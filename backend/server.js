23e68693fb477fece1293585d1be7d1aa9d5f5ef
spoorthi <Spoorthi.L@ellucian.com>
2025-03-03 16:06:42 +0530

    Login page finished ; need to encrypt

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,7 @@ const http = require('http');
 const WebSocket = require('ws');
 const oracledb = require('oracledb');
 const cors = require('cors');
-<<<<<<< HEAD
 require('dotenv').config();
-=======
-const { log } = require('console');
-require("dotenv").config();
->>>>>>> c2635c1c76f2422e423636c4f7bed3f93946f293
 
 const dbConfig = {
   user: process.env.DB_USER,
@@ -27,13 +22,10 @@ wss.on('connection', (ws) => {
   console.log('New client connected');
 
   ws.on('message', async (message) => {
-<<<<<<< HEAD
-    const { username, password } = JSON.parse(message); 
-=======
+
     const { name, password } = JSON.parse(message);
     console.log(name,password);
     
->>>>>>> c2635c1c76f2422e423636c4f7bed3f93946f293
     try {
       let connection = await oracledb.getConnection(dbConfig);
       console.log("Successfully connected to the database");
@@ -41,11 +33,7 @@ wss.on('connection', (ws) => {
         `SELECT * FROM ADMIN_USERS WHERE USERNAME = :name AND PASSWORD = :password AND ROLE = 'ADMIN'`,
         [name, password]
       );
-<<<<<<< HEAD
-        console.log(result);
-=======
       console.log("result:",result)
->>>>>>> c2635c1c76f2422e423636c4f7bed3f93946f293
       if (result.rows.length > 0) {
         ws.send(JSON.stringify({ success: true, message: 'Login successful' }));
       } else {
